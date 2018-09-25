d6f97b5ce6fc99f59cccc79f13fd6110bb991b05
lessonzhang <lessonzhang@qq.com>
2018-09-25 15:58:33 +0800

    change tncoin path

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     keystoreUrl : 'http://localhost:3000/keystore/',
     localRPC : 'http://192.168.23.125:8545',
-    TNCoinPath : 'build/contracts/TNCoin.json'
+    TNCoinPath : '../build/contracts/TNCoin.json'
 }
